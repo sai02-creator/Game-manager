eacda0a124afff945399284e6c9d95192c38b4d8
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-09-19 01:04:28 +1000

    Add Navbar component with Home and Add Game links

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
-    return<div></div>
+    return(
+    <div className="navbar">
+        <div className="navbar-title">MyGameSite</div>
+        <div className="navbar-links">
+        <Link to="/"> Home </Link>
+                <Link to="/create"> Add Game </Link>
+       </div>
+
+    </div>
+    );
 }
 
 
