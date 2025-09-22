a913890b5141b91411ae6ba2edc9514c7f31ae97
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-09-23 00:06:42 +1000

    Add CreateGame component with form for adding new games

--- client/src/pages/CreateGame.jsx
+++ client/src/pages/CreateGame.jsx
@@ -1,6 +1,20 @@
 
 function CreateGame() {
-    return<div>CreateGame</div>
+    return<div className="page-container">
+        <h2>Add A New Game</h2>
+        <form className="form-container bg-card" style={{padding: "1rem"}}>
+            <label> Game Name </label>
+            <input placeholder="E.g. Super Mario"/>
+
+               <label> Platform </label>
+            <input placeholder="E.g. Switch, PC"/>
+
+               <label> Genre </label>
+            <input placeholder="E.g. RPG, Action, Puzzle"/>
+
+            <button type="submit"> Create</button>
+        </form>
+    </div>
 } 
 
 export default CreateGame;
