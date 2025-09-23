336edf069c5de37ace5ac6206f55c193c177ee71
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-09-23 23:48:34 +1000

    Add onSubmit handler to send form data using react-query mutate

--- client/src/pages/CreateGame.jsx
+++ client/src/pages/CreateGame.jsx
@@ -1,16 +1,54 @@
+import { useForm } from "react-hook-form";
+
+import { useMutation } from "@tanstack/react-query";
+
+async function createGame(newGame) {
+    const response = await fetch("http://localhost:3000/api/games", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(newGame)
+    })
+
+    return response.json()
+}
+
 
 function CreateGame() {
+    const {register, handleSubmit, formState: {errors}} = useForm();
+
+
+    const { mutate } = useMutation({mutationFn: createGame })
+
+
+
+    const onSubmit = (formData) => {
+        console.log(formData)
+        mutate({
+            name: formData.name,
+            platform: formData.platform,
+            genre: formData.genre,
+        })
+    };
+
     return<div className="page-container">
         <h2>Add A New Game</h2>
-        <form className="form-container bg-card" style={{padding: "1rem"}}>
+        <form onSubmit={handleSubmit(onSubmit)} className="form-container bg-card" style={{padding: "1rem"}}>
             <label> Game Name </label>
-            <input placeholder="E.g. Super Mario"/>
+            <input placeholder="E.g. Super Mario" {...register("name", { required: "Name is required." })}/>
+
+            {errors.name && <p style={{ color: "red" }}> {errors.name.message}</p>}
 
                <label> Platform </label>
-            <input placeholder="E.g. Switch, PC"/>
+            <input placeholder="E.g. Switch, PC" {...register("platform", { required: "Platform is required." })}/>
+
+                        {errors.platform && <p style={{ color: "red" }}> {errors.platform.message}</p>}
+
 
                <label> Genre </label>
-            <input placeholder="E.g. RPG, Action, Puzzle"/>
+            <input placeholder="E.g. RPG, Action, Puzzle" {...register("genre", { required: "Genre is required." })}/>
+
+                        {errors.genre && <p style={{ color: "red" }}> {errors.genre.message}</p>}
+
 
             <button type="submit"> Create</button>
         </form>
