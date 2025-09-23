c9e3b387380a18dccda0dbeaf67ba64a996e10cf
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-09-23 23:51:32 +1000

    Convert server folder from submodule to normal folder

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import cors from 'cors'
+import { games } from './fakeDB.js'
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.get("/api/games", (req, res) => {
+    res.json(games)
+});
+
+app.post("/api/games", (req, res) => {
+    const newGame = req.body
+    games.push({...newGame, id: games.length + 1 });
+    res.status(201).json(newGame);
+})
+
+
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000")
+});
