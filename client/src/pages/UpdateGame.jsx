e9e22635d69cd8e6fe83069445466cbc2f37c4ed
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-09-26 00:30:22 +1000

    Add UpdateGame form with react-hook-form for editing game details

--- client/src/pages/UpdateGame.jsx
+++ client/src/pages/UpdateGame.jsx
@@ -1,5 +1,36 @@
+import { useParams } from "react-router-dom";
+import { useForm} from "react-hook-form";
+
 function UpdateGame() {
-    return<div>UpdateGame</div>
+    const { id } = useParams();
+
+        const {register, handleSubmit, formState: {errors}, reset,} = useForm();
+    
+    return(
+    <div className="page-container">
+        <h2> Update Game (ID: {id})</h2>
+        <form onSubmit={handleSubmit(onSubmit)} className="form-container bg-card" style={{padding: "1rem"}}>
+            <label> Game Name </label>
+            <input placeholder="E.g. Super Mario" {...register("name", { required: "Name is required." })}/>
+
+            {errors.name && <p style={{ color: "red" }}> {errors.name.message}</p>}
+
+               <label> Platform </label>
+            <input placeholder="E.g. Switch, PC" {...register("platform", { required: "Platform is required." })}/>
+
+                        {errors.platform && <p style={{ color: "red" }}> {errors.platform.message}</p>}
+
+
+               <label> Genre </label>
+            <input placeholder="E.g. RPG, Action, Puzzle" {...register("genre", { required: "Genre is required." })}/>
+
+                        {errors.genre && <p style={{ color: "red" }}> {errors.genre.message}</p>}
+
+
+            <button type="submit"> Create</button>
+        </form>
+    </div>
+    );
 } 
 
 export default UpdateGame;
