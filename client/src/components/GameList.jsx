cec92ecb73a3ac1eec3737db303d2202ec8c9785
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-10-01 00:03:48 +1000

    feat(games): add delete mutation with query invalidation in GameList

--- client/src/components/GameList.jsx
+++ client/src/components/GameList.jsx
@@ -1,7 +1,33 @@
 import { Link } from "react-router-dom";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+
+    async function deleteGame(id) {
+        const response = await fetch(`http://localhost:3000/api/games/${id}`, {
+        method: "DELETE",
+      
+        })
+        return response.json()
+        }; 
 
 function GameList(props) {
     const games = props.games;
+    const queryClient = useQueryClient()
+
+    const { mutate } = useMutation({mutationFn: deleteGame, onSuccess: () => {
+        queryClient.invalidateQueries(["games"]);
+    }
+    })
+
+    const handelDelete = (id) => {
+        if (window.confirm("Are you sure you want to delete this game?")){
+            mutate(id);
+        }
+
+    }
+
+
+
     return( <div className="games-grid">{games.map((game,key) => (
         <div key={key} className="game-card">
             <h3>{game.name}</h3>
@@ -9,6 +35,7 @@ function GameList(props) {
             <p>{game.genre}</p>
             <div>
                 <Link to={`/update/${game.id}`}>Edit</Link>
+                <button onClick={() => handelDelete(game.id)}> Delete </button>
             </div>
             
         </div>
